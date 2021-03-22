fbe746fdb507a1fe0be2b459e643c5200de38613
Akshad Agrawal <akshadagrawal.aa@gmail.com>
2021-03-22 20:56:48 +0530

    lect 5+6

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const http= require('http');
 const fs = require('fs');
+const _ = require('lodash');
 
 const server= http.createServer((req,res)=>{
     
@@ -53,6 +54,19 @@ const server= http.createServer((req,res)=>{
         }
     })
 
+
+    //========lodash=========
+
+    const num =_.random(0,20);
+    console.log(num);
+
+    const greet = _.once(()=>{
+        console.log('hello');
+    });
+
+    greet();
+    greet(); //only once would be run
+    
 });
 
 
