9ad45126c9aa882ef2ceba7ac85d1ee2788864ee
Akshad Agrawal <akshadagrawal.aa@gmail.com>
2021-03-23 13:30:38 +0530

    lect 7 done

--- app.js
+++ app.js
@@ -1,7 +1,52 @@
+//============express app=================================================
+
+// const express = require('express');
+
+// //express app
+// const app= express();
+
+
+// //listen for requests
+// app.listen(3000, (err)=>{
+//     console.log('listening to 3000');
+// });
+
+// app.get('/', (req,res)=>{  
+//     //res.send('<p>Hello guys!!</p>')
+//     res.sendFile('./veiws/index.html', {root: __dirname});
+// });
+// app.get('/about', (req,res)=>{  
+//     res.sendFile('./veiws/about.html', {root: __dirname});
+// });
+
+
+// //redirects
+
+// app.get('/about-me', (req,res)=>{
+//     res.redirect('/about');
+// })
+
+
+// //404 pages //this needs to be at the end of all others since it is default and used in every case
+
+// app.use((req,res)=>{
+//     res.status(404).sendFile('./veiws/404.html', {root: __dirname})
+// })
+
+
+
+//================express app along with view engines
+
+
 const express = require('express');
 
 //express app
 const app= express();
+
+//register view engine
+app.set('view engine', 'ejs');
+
+//app.set('views','some another name for views')
 
 
 //listen for requests
@@ -10,23 +55,21 @@ app.listen(3000, (err)=>{
 });
 
 app.get('/', (req,res)=>{  
-    //res.send('<p>Hello guys!!</p>')
-    res.sendFile('./views/index.html', {root: __dirname});
+    const blogs=[
+        {title: 'Akshad ', snippet: 'Lorem  nubj,ey bkdjc dbjcfh bhjcxz'},
+        {title: 'Abhishek ', snippet: 'Lorem  nubj,ey bkdjc dbjcfh bhjcxz'},
+        {title: 'Anant ', snippet: 'Lorem  nubj,ey bkdjc dbjcfh bhjcxz'}
+    ];
+
+    res.render('index', {title: 'Home' , blogs}); //send dynamic contet through {} curly braces
 });
 app.get('/about', (req,res)=>{  
-    res.sendFile('./views/about.html', {root: __dirname});
+    res.render('about', {title: 'About'});
+});
+app.get('/blogs/create', (req,res)=>{  
+    res.render('create', {title: 'Create'});
 });
 
-
-//redirects
-
-app.get('/about-me', (req,res)=>{
-    res.redirect('/about');
-})
-
-
-//404 pages //this needs to be at the end of all others since it is default and used in every case
-
 app.use((req,res)=>{
-    res.status(404).sendFile('./views/404.html', {root: __dirname})
-})
+    res.render('404', {title: 'Error'});
+}) 
